28be218099dfc0fdaffcc7aa62b50e5aec425523
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-29 16:03:03 -0500

    created 3d logo

--- src/components/canvas/Logo.tsx
+++ src/components/canvas/Logo.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { useThree, Canvas } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
-import { OrbitControls } from '@react-three/drei';
+// import { OrbitControls } from '@react-three/drei';
 
 const Logo = () => {
   const { scene, camera } = useThree();
@@ -13,8 +13,8 @@ const Logo = () => {
     const loader = new GLTFLoader();
     loader.load('/3d-models/logo.glb', (gltf) => {
       const model = gltf.scene as THREE.Group; // Cast to THREE.Group
-      model.scale.set(7, 7, 7); // Adjust the scaling factor as needed
-      model.position.set(-4, 7, 0); // Adjust the initial position
+      model.scale.set(1, 1, 1); 
+      model.position.set(-2, 1, 0); 
       model.traverse((child) => {
         if ((child as THREE.Mesh).isMesh) {
           child.castShadow = true;
@@ -27,12 +27,12 @@ const Logo = () => {
       console.error('Error loading GLTF model:', error);
     });
 
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(-4, 3, 6);
+    const light = new THREE.DirectionalLight(0xffffff, 5);
+    light.position.set(3, 3, 3);
     light.castShadow = true;
     scene.add(light);
 
-    scene.background = new THREE.Color(0xffffff);
+    // scene.background = new THREE.Color(0xffffff);
 
     return () => {
       // Remove the loaded model when the component unmounts
@@ -46,10 +46,10 @@ const Logo = () => {
 };
 
 const ThreeCanvasWithLogo = () => (
-  <div className="relative w-40">
-    <Canvas className="absolute top-0 left-0 w-full h-full" shadows camera={{ position: [0, 0, 10] }}>
+  <div className="relative w-50">
+    <Canvas className="absolute top-0 left-0 w-full h-full" shadows camera={{ position: [0, 0, 5] }}>
       <Logo />
-      <OrbitControls />
+      {/* <OrbitControls /> */}
     </Canvas>
   </div>
 );
