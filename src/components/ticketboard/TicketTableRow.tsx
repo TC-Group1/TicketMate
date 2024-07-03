11dfe900f79827798a4f11b913c550ed817e48b0
Jonathan Potter <jonathan.d.potter@outlook.com>
2024-07-03 16:09:16 -0400

    Added DetailsCard modal to TicketTableRow, removed global button css

--- src/components/ticketboard/TicketTableRow.tsx
+++ src/components/ticketboard/TicketTableRow.tsx
@@ -1,23 +1,41 @@
+"use client";
+
 import { Ticket } from "@/types";
-import { FC } from "react";
+import { FC, useState } from "react";
+import Modal from "@/components/modals/modal";
+import DetailsCard from "../modals/DetailsCard";
 
 const TicketTableRow: FC<{ ticket: Ticket }> = ({ ticket }) => {
+  const [detailsOpen, setDetailsOpen] = useState<boolean>(false);
+
   return (
-    <tr>
-      <td>{ticket.title}</td>
-      <td>{ticket.assignees ? ticket.assignees[0] : "unassigned"}</td>
-      <td>{ticket.priority}</td>
-      <td>{ticket.dateCreated}</td>
-      <td>
-        <select defaultValue={ticket.status} title="status">
-          <option value="New">New</option>
-          <option value="In Progress">In Progress</option>
-          <option value="Completed">Complete</option>
-        </select>
-      </td>
-      <td>{ticket.createdBy}</td>
-      <td className="w-[90px]">{ticket.lastModified}</td>
-    </tr>
+    <>
+      <tr>
+        <td>
+          <button type="button" onClick={() => setDetailsOpen(true)}>
+            {ticket.title}
+          </button>
+        </td>
+        <td>{ticket.assignees ? ticket.assignees[0] : "unassigned"}</td>
+        <td>{ticket.priority}</td>
+        <td>{ticket.dateCreated}</td>
+        <td>
+          <select defaultValue={ticket.status} title="status">
+            <option value="New">New</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Completed">Complete</option>
+          </select>
+        </td>
+        <td>{ticket.createdBy}</td>
+        <td className="w-[90px]">{ticket.lastModified}</td>
+      </tr>
+      <Modal
+        isOpen={detailsOpen}
+        setIsOpen={setDetailsOpen}
+        closeButton
+        children={<DetailsCard {...{ ticket }} />}
+      />
+    </>
   );
 };
 export default TicketTableRow;
