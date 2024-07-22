357a7eacb0940d41f0e1c07a7c974393d8ef92ff
Nichole McGrew <nicholemcgrew@gmail.com>
2024-07-22 18:17:30 -0500

    converted css to tailwind, fixed errors, and made code more efficient

--- src/app/(without-menu)/login/page.tsx
+++ src/app/(without-menu)/login/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, FC, useRef } from "react"
 import { useUserContext } from "../../../features/user/UserContextProvider"
-import { UserContext } from "../../../types"
+import { UserContextType } from "../../../types"
 
 // Modal additions
 import Modal from "@/components/modals/modal"
@@ -23,18 +23,18 @@ const LoginPage: FC = () => {
 
 	const showErrorRef = useRef<boolean>(false)
 
-	const userContext: UserContext | null = useUserContext()
+	const userContext: UserContextType | null = useUserContext()
 
 	const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setUsername(event.target.value)
-		setError(false);
-		showErrorRef.current = false;
+		setError(false)
+		showErrorRef.current = false
 	}
 
 	const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setPassword(event.target.value)
-		setError(false);
-		showErrorRef.current = false;
+		setError(false)
+		showErrorRef.current = false
 	}
 
 	function handleSubmit(
@@ -44,9 +44,9 @@ const LoginPage: FC = () => {
 
 		if (username === "" || password === "") {
 			setUserNotification("Please enter a username and password")
-			setError(true);
-			showErrorRef.current = true;
-      return;
+			setError(true)
+			showErrorRef.current = true
+			return
 		}
 
 		if (username && password) {
@@ -71,12 +71,12 @@ const LoginPage: FC = () => {
 				}
 			}
 
-      if (!passwordRegex.test(password)) {
-        setError(true);
-        showErrorRef.current = true;
-        setUserNotification("Password does not meet the required criteria.");
-        return;
-      }
+			if (!passwordRegex.test(password)) {
+				setError(true)
+				showErrorRef.current = true
+				setUserNotification("Password does not meet the required criteria.")
+				return
+			}
 
 			userContext?.useLoginSubmission(usernameUpdate, password)
 			console.log("Trying to login")
@@ -84,57 +84,72 @@ const LoginPage: FC = () => {
 	}
 
 	return (
-		<div className="grid place-items-center bg-gray-100 text-center h-screen">
-			<div className="w-[330px] p-[40px_30px] bg-white rounded-lg shadow-md">
-				<div className="text-2xl font-semibold mb-9 text-gray-600">Ticketmate Login</div>
+		<div className="grid place-items-center bg-bg-primary text-center h-screen">
+			<div className="w-[330px] p-[40px_30px] bg-login-background rounded-[10px] shadow-neumorphism">
+				<div className="text-text-lg font-semibold mb-[35px] text-gray-dark">
+					Ticketmate Login
+				</div>
 				<form>
-					<div className="h-[50px] w-full flex relative mb-5">
-          {username.length > 0 && <label className="absolute top-1/2 transform -translate-y-1/2 left-6 bg-white text-gray-500">Email or Phone</label>}
+					<div className="relative flex h-field-height w-full">
+						{username.length > 0 && (
+							<label className="absolute top-1/2 -translate-y-[150%] left-[10px] bg-login-background text-field-label-text pointer-events-none">
+								Email or Phone
+							</label>
+						)}
 						<input
 							type="text"
 							name="username"
 							value={username}
 							onChange={handleUsernameChange}
 							placeholder="Email or Phone"
-							className={`h-full w-full pl-[45px] outline-none border-none text-lg bg-white text-gray-600 rounded-full shadow-inner ${showErrorRef.current ? 'border-2 border-red-500' : ''}`}
+							className={`h-full w-full pl-[10px] outline-none border-none text-lg bg-login-background text-field-input-text rounded-field-input shadow-field-input ${
+								showErrorRef.current ? "border-error" : ""
+							}`}
 						/>
-						<span className="absolute text-gray-600 w-12 leading-[50px] left-0 pl-4">👤</span>
+						<span className="fas fa-user"></span>
 					</div>
-					<div className="h-[50px] w-full flex relative mb-5">
-						{password.length > 0 && <label>Password</label>}
+					<div className="relative flex h-field-height w-full mt-4">
+						{password.length > 0 && (
+							<label className="absolute top-1/2 -translate-y-[150%] left-[10px] bg-login-background text-field-label-text pointer-events-none">
+								Password
+							</label>
+						)}
 						<input
 							type="password"
 							name="password"
 							value={password}
 							onChange={handlePasswordChange}
 							placeholder="Password"
-							className={`h-full w-full pl-[45px] outline-none border-none text-lg bg-white text-gray-600 rounded-full shadow-inner ${showErrorRef.current ? 'border-2 border-red-500' : ''}`}
+							className={`h-full w-full pl-[10px] outline-none border-none text-lg bg-login-background text-field-input-text rounded-field-input shadow-field-input ${
+								showErrorRef.current ? "border-error" : ""
+							}`}
 						/>
-						<span className="absolute text-gray-600 w-12 leading-[50px] left-0 pl-4">🔒</span>
+						<span className="fas fa-lock"></span>
 					</div>
 					<div>
 						{showErrorRef.current && (
-							 <p className="text-red-500 text-sm text-left mb-5">
-               {userNotification}
-             </p>
+							<p className="text-red-500 text-sm text-left">
+								Invalid username or password
+							</p>
 						)}
 					</div>
-					<div className="text-left mb-5 ml-2">
-						<a href="#" className="text-lg text-purple-600 hover:underline">Forgot Password?</a>
+					<div className="text-left mt-2.5 ml-1.5">
+						<a href="#" className="text-light-purple text-base hover:underline">
+							Forgot Password?
+						</a>
 					</div>
 					<button
-						className="my-4 w-full h-12 text-lg font-semibold bg-purple-600 rounded-full shadow text-white hover:bg-purple-700 focus:outline-none"
-            onClick={handleSubmit}
+						className="my-4 w-full h-12 text-lg font-semibold bg-light-purple rounded-3xl shadow text-white focus:bg-dark-purple focus:shadow-inner"
+						onClick={handleSubmit}
 					>
 						Sign in
 					</button>
 				</form>
 
-				<div className="my-5 text-gray-600 text-lg">
+				<div className="mt-4">
 					Don&apos;t have an account?
 					<button
 						id="signup-btn"
-            className="ml-2 text-purple-600 hover:underline"
 						onClick={() => setRegistrationModalOpen(true)}
 					>
 						Sign up now
@@ -147,23 +162,28 @@ const LoginPage: FC = () => {
 				</div>
 
 				{userNotification && (
-					<div className="border border-red-500 p-2 rounded-md bg-gray-100">
-          <h2 className="text-red-500 text-sm">{userNotification}</h2>
-        </div>
+					<div
+						className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4"
+						role="alert"
+					>
+						<h2 className="text-red-700 font-semibold">{userNotification}</h2>
+					</div>
 				)}
 			</div>
 			<div>
-				{/* null check user context, then check if there's an error.
-        If there's an error, render it to the UI */}
-				{userContext && userContext.error && (
-						<div className="border border-red-500 p-2 rounded-md bg-gray-100">
-							<h2 className="text-red-500 text-sm">{userContext.error}</h2>
-						</div>
-				)}
-			</div>
-		</div>
-	)
-}
+        {userContext?.error && (
+          <div
+            className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4"
+            role="alert"
+          >
+            <h2 className="text-red-700 font-semibold">{userContext.error}</h2>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
+        
 
 export default LoginPage
