f99271e55d563d9b6b72432471488288f1380df0
Jonathan Potter <jonathan.d.potter@outlook.com>
2024-08-05 16:37:18 -0400

    Updated dashboard to redirect to login page if user is undefined

--- src/app/(with-menu)/dashboard/page.tsx
+++ src/app/(with-menu)/dashboard/page.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { FC } from "react";
 import TicketBoard from "@/components/ticketboard/TicketBoard";
 import TicketBoardTeam from "@/components/ticketboard/TicketBoardTeam";
 import data from "@/mockData.json";
+import { useRouter } from "next/navigation";
 
 const Dashboard: FC = () => {
   const mockData = data;
+  const router = useRouter();
+
+  useEffect(() => {
+    !mockData.user && router.push("/login");
+  }, [mockData]);
 
   return (
     <div className="block border-t border-[0xc8cbd9] px-5 w-full">
